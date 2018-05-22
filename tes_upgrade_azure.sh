894a0dab461af44fd2f405716b27bb9d52c1fcf2
root <root@localhost.localdomain>
2018-05-22 14:01:11 -0700

    Updated script

---
+++ tes_upgrade_azure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+service thingserver stop
+echo -n "What's the Upgrade  Version?  "
+read ver
+mkdir /appl/tes_$ver
+cp /home/ftashev/thingserver-8.2.3-b576.zip /appl/tes_$ver
+cd /appl/tes_$ver && unzip thingserver-8.2.3-b576.zip && rm -rf thingserver-8.2.3-b576.zip
+mv /appl/tes_$ver/configuration.json /appl/tes_$ver/configuration.json_old && cp /appl/tes/configuration.json /appl/tes_$ver
+cp -a /appl/tes/stores/projects /appl/tes_$ver/stores
+# npm install
+chown -R root.twadmin /appl/tes_$ver
+rm -rf /appl/tes && ln -s /appl/tes_$ver /appl/tes
+service thingserver start
+systemctl stop tomcat
+systemctl status tomcat 
+systemctl start tomcat 
+  
